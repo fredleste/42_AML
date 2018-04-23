f26a10f8cc122afb6a1be0db4e8d85d31184f392
Frederic LESTE-LASSERRE <fleste-l@student.42.fr>
2018-04-23 19:39:20 +0200

    linear regression function

---
+++ ft_linear_regression/linreg.py
@@ -0,0 +1,35 @@
+#!/sgoinfre/goinfre/Perso/fleste-l/anaconda3/bin/python
+
+import pandas as pd
+
+data = pd.read_csv('ftlr_data1.csv')
+
+data['kmfs'] = (data['km'] - min(data['km'])) / (max(data['km']) - min(data['km']))
+data['pricefs'] = (data['price'] - min(data['price'])) / (max(data['price']) - min(data['price']))
+
+X = data['kmfs'].values
+y = data['pricefs'].values
+
+def predict(X, theta):
+    return theta[0] + theta[1] * X
+
+km = -1
+while km < 0:
+	km = input("Enter the car mileage (km) :\n")
+	try:
+		km = int(km)
+	except ValueError:
+		print("Nothing entered")
+		km = -1
+		continue
+	if km < 0:
+		print("Negative mileage")
+
+theta = [0.93931892944579276, -1.0035757423865808]
+kmfs = (km - min(data['km'])) / (max(data['km']) - min(data['km']))
+resfs = theta[0] + (theta[1] * kmfs)
+if resfs == 0:
+	print("The car price is 0.0 euro")
+else:
+	res = (resfs * (max(data['price']) - min(data['price']))) + min(data['price'])
+	print("The car price is", res,"euros")
