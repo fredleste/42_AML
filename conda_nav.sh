25a2b6257316fda5de4a830e8110071945a5844c
Frederic LESTE-LASSERRE <fleste-l@e3r12p4.42.fr>
2018-06-26 19:03:44 +0200

    all files

--- conda_nav.sh
+++ conda_nav.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sh /sgoinfre/goinfre/Perso/fleste-l/anaconda3/Anaconda-Navigator.app/Contents/MacOS/run.sh
+sh /Volumes/San447/42goinfre/anaconda3/Anaconda-Navigator.app/Contents/MacOS/run.sh
